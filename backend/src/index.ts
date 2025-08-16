da4cb120fb4b1026c493e5a562e08e990d1b5e43
Karansri123 <karansrivastava189@gmailcom>
2025-08-16 18:24:47 +0530

    basic setup

---
+++ backend/src/index.ts
@@ -0,0 +1,55 @@
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import session from "cookie-session";
+import { config } from "./config/app.config";
+import connectDatabase from "./config/database.config";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
+import { HTTPSTATUS } from "./config/http.config";
+import { error } from "console";
+import { asyncHandler } from "./middlewares/asyncHandler.middleware";
+import { BadRequestException } from "./utils/appError";
+import { ErrorCodeEnum } from "./enums/error-code.enum";
+
+const app = express();
+const BASE_PATH = config.BASE_PATH;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    name: "session",
+    keys: [config.SESSION_SECRET],
+    maxAge: 24 * 60 * 60 * 1000, // 1 day,
+    secure: config.NODE_ENV === "production",
+    httpOnly: true,
+    sameSite: "lax",
+  })
+);
+
+app.use(
+  cors({
+    origin: config.FRONTEND_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.get(
+  "/",
+  asyncHandler(async(req: Request, res: Response, next: NextFunction) => {
+    throw new BadRequestException("This is bad request",ErrorCodeEnum.AUTH_INVALID_TOKEN)
+    res.status(HTTPSTATUS.OK).json({
+      message: "Welcome to the TASKHUB",
+    });
+  })
+);
+app.use(errorHandler);
+
+app.listen(config.PORT, async () => {
+  console.log(
+    `Server is running on port ${config.PORT} in ${config.NODE_ENV} mode`
+  );
+  // await connectDB();
+  await connectDatabase();
+});
---
+++ backend/src/middlewares/errorHandler.middleware.ts
@@ -0,0 +1,28 @@
+import { ErrorRequestHandler } from "express";
+import { HTTPSTATUS } from "../config/http.config";
+import { AppError } from "../utils/appError";
+
+export const errorHandler: ErrorRequestHandler = (
+  error,
+  req,
+  res,
+  next
+): any => {
+  console.error(`Error occured on Path: ${req.path}`, error);
+  if (error instanceof SyntaxError) {
+    return res
+      .status(HTTPSTATUS.BAD_REQUEST)
+      .json({ message: "Invalid JSON format.please check your request" });
+  }
+
+  if (error instanceof AppError) {
+    return res
+      .status(error.statusCode)
+      .json({ message: error.message, errorCode: error.errorCode });
+  }
+
+  return res.status(HTTPSTATUS.INTERNAL_SERVER_ERROR).json({
+    message: "Internal server error",
+    error: error?.message || "unknow error occured",
+  });
+};
