3b30e777d281e6a9cf62c75b9283dda447628a12
Karansri123 <karansrivastava189@gmailcom>
2025-08-22 17:01:33 +0530

    add login & logout endpoints

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,24 +1,34 @@
 import { Router } from "express";
 import passport from "passport";
 import { config } from "../config/app.config";
-import { googleLoginCallBack, registerUserController } from "../controllers/auth.controller";
-
+import {
+  googleLoginCallBack,
+  loginController,
+  logOutController,
+  registerUserController,
+} from "../controllers/auth.controller";
 
 const failedUrl = `${config.FRONTEND_GOOGLE_CALLBACK_URL}?status=failure`;
 
 const authRoutes = Router();
 
-authRoutes.post("/register",registerUserController);
+authRoutes.post("/register", registerUserController);
+authRoutes.post("/login", loginController);
+authRoutes.post("/logout", logOutController);
 
-authRoutes.get("/google",passport.authenticate("google",{
-    scope:["profile","email"],
-}));
-
-authRoutes.get("/google/callback",passport.authenticate("google",{
-    failureRedirect:failedUrl,
-}),
-googleLoginCallBack
+authRoutes.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
 );
 
+authRoutes.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: failedUrl,
+  }),
+  googleLoginCallBack
+);
 
 export default authRoutes;
